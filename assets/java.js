b6bbaf7b8a7f01576ce83a7723efcdf78cf3179b
tushar64204 <tushar64204@gmail.com>
2024-03-24 16:28:38 +0530

    happyholi1

---
+++ assets/java.js
@@ -0,0 +1,39 @@
+function editDetails() {
+    var name = prompt("Enter your name:");
+    if (name !== null) {
+      document.getElementById("user-name").innerText = name;
+    }
+  
+    var photoURL = prompt("Enter URL of your photo:");
+    if (photoURL !== null) {
+      document.getElementById("user-photo").src = photoURL;
+    }
+  }
+  var elements = document.getElementsByTagName("div");
+window.setInterval(function(){
+  setUp();
+}, 300);
+for(var i = 0; i<elements.length; i++)
+	{
+		elements[i].addEventListener("click", function(){
+		setUp();
+		});
+	}
+
+function setUp(){
+
+for(var i = 0, length = elements.length; i < length; i++) {
+       if( elements[i].textContent == ''){
+          elements[i].style.backgroundColor = randomColor();
+       } 
+    }
+}
+function randomColor() 
+{
+	var r, g,b;
+	r = Math.floor(Math.random() * 256);
+	g = Math.floor(Math.random() * 256);
+	b = Math.floor(Math.random() * 256);
+	var rgb = "rgb("+r+", "+g+", "+b+")";
+	return rgb;
+}
